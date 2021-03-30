36c6abbb7438741e4d90bd38ac33209cf693d8ac
Mitchell Rodríguez <mitrodle@gmail.com>
2021-03-30 02:54:46 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { VideoCard } from './components/Video/VideoCard';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    const classes = useStyles();
+
+    const [questions] = useState(['¿Cuál es tu animal favorito?', '¿Cuál es tu color favorito?', '¿Cuál es tu comida favorita?', '¿Cuál es tu planta favorita?']);
+    
+    return (
+        <Container fixed>
+            <div className={classes.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>Video Cuestionario</Paper>
+                    </Grid>
+                    {
+                        questions.map((question, index) => {
+                            return (
+                                <Grid item xs={12} sm={3}>
+                                    <VideoCard key={`videoCard-${index}`} question = {question}/>
+                                </Grid>
+                            )
+                        })
+                    }
+                </Grid>
+            </div>
+        </Container>
+    )
 }
 
-export default App;
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        overflow: "hidden"
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: "center",
+        color: theme.palette.text.primary,
+    }
+}));
